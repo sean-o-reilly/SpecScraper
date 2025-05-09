f72eeecccbaa7f04239ffbaa180c790083e4f404
Sean OReilly <oreilly.e.sean@gmail.com>
2025-05-09 16:08:33 -0500

    started streamlit control flow

--- demo.py
+++ demo.py
@@ -1,20 +1,18 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
 
-gpu_data = {
-    "GTX 1060": {"VRAM": 6, "Cores": 1280},
-    "RTX 5070": {"VRAM": 12, "Cores": 7168}
-}
 
-gpu1, gpu2 = st.selectbox("Select GPU 1", gpu_data.keys()), st.selectbox("Select GPU 2", gpu_data.keys())
 
-labels = list(gpu_data[gpu1].keys())
-x = range(len(labels))
+st.title("Hello, welcome to SpecScraper!")
 
-fig, ax = plt.subplots()
-ax.bar(x, [gpu_data[gpu1][k] for k in labels], width=0.4, label=gpu1)
-ax.bar([i+0.4 for i in x], [gpu_data[gpu2][k] for k in labels], width=0.4, label=gpu2)
-ax.set_xticks([i+0.2 for i in x])
-ax.set_xticklabels(labels)
-ax.legend()
-st.pyplot(fig)
+st.write("Enter two GPU's to compare below..")
+
+gpu_1 = st.text_input("GPU 1:")
+gpu_2 = st.text_input("GPU 2:")
+
+
+if gpu_1.strip() and gpu_2.strip():     #.strip() will ignore whitespace, this control flow will only trigger once both parameters are entered
+    if st.button(f"Compare {gpu_1} and {gpu_2}"): # button might be unnessacararary or however you spell it
+        st.write(f"Comparing {gpu_1} and {gpu_2}")
+
